40e1265d8c8187da8b8e9a72a9aeaa0899732f19
Luis Gonzalez <luismaglz@live.com>
2018-09-23 11:50:21 -0600

    added a few things to make calculations easier

--- goldPodRacing.js
+++ goldPodRacing.js
@@ -1,8 +1,18 @@
+"use strict";
 ///<reference path="definitions.d.ts" />
 class RaceInfo {
     constructor() {
+        this.frames = 0;
         this.laps = 0;
         this.checkPoints = [];
+        this.checkPointCount = 0;
+        this.lastCheckPointId = 0;
+    }
+    checkPoint(index) {
+        return this.checkPoints[index];
+    }
+    nextCheckPoint(pod) {
+        return this.checkPoints[pod.nextCheckPointId];
     }
 }
 class Point {
@@ -18,15 +28,28 @@ class PodTracking {
     }
 }
 class Pod extends Point {
-    constructor() {
-        super(...arguments);
+    constructor(id, x = '0', y = '0', sx = '0', sy = '0', angle = '0', nextId = '0') {
+        super(x, y);
+        this.id = 0;
         this.speedX = 0;
         this.speedY = 0;
+        this.boundDelta = 90;
         this.angle = 0;
         this.nextCheckPointId = 0;
+        this.id = id;
+        this.speedX = parseInt(sx);
+        this.speedY = parseInt(sy);
+        this.angle = parseInt(angle);
+        this.nextCheckPointId = parseInt(nextId);
+        // this.setVisibleRange();
     }
     moveToPoint(x, y, thrust) {
         return print(`${x} ${y} ${thrust}`);
+    }
+    hasTargetInFront(target) {
+        var targetAngle = HelperMethods.getRelativeAngle(this, target) + 360;
+        var anglediff = (this.angle - targetAngle + 180 + 360) % 360 - 180;
+        return (anglediff <= this.boundDelta && anglediff >= -this.boundDelta);
     }
 }
 ;
@@ -47,6 +70,18 @@ class NextCheckPoint extends Point {
 }
 ;
 class HelperMethods {
+    static initializePods() {
+        var podTracking = new PodTracking();
+        for (var i = 0; i < 2; i++) {
+            var inputs = readline().split(' ');
+            podTracking.myPods.push(new Pod(i, inputs[0], inputs[1], inputs[2], inputs[3], inputs[4], inputs[5]));
+        }
+        for (var i = 0; i < 2; i++) {
+            var inputs = readline().split(' ');
+            podTracking.enemyPods.push(new Pod(i + 2, inputs[0], inputs[1], inputs[2], inputs[3], inputs[4], inputs[5]));
+        }
+        return podTracking;
+    }
     static getDistanceBetween(pod1, pod2) {
         var a = pod1.positionX - pod2.positionX;
         var b = pod1.positionY - pod2.positionY;
@@ -58,6 +93,32 @@ class HelperMethods {
         var angleOfAttack = Math.atan(checkPointRadius / distanceToCheckpoint) * (180 / Math.PI);
         return angleOfAttack;
     }
+    static getRelativeAngle(pod, target) {
+        var deltaX = pod.positionX - target.positionX;
+        var deltaY = pod.positionY - target.positionY;
+        var absDeltaX = Math.abs(pod.positionX - target.positionX);
+        var absDeltaY = Math.abs(pod.positionY - target.positionY);
+        // var angle = Math.atan(opposite / adjacent) * (180 / Math.PI);
+        var angle = Math.atan(absDeltaY / absDeltaX) * (180 / Math.PI);
+        var targetAngle = 0;
+        if (deltaX < 0 && deltaY < 0) {
+            // top left
+            targetAngle = angle;
+        }
+        else if (deltaX < 0 && deltaY > 0) {
+            // bottom left
+            targetAngle = angle + 270;
+        }
+        else if (deltaX > 0 && deltaY > 0) {
+            // bottom right
+            targetAngle = angle + 180;
+        }
+        else if (deltaX > 0 && deltaY < 0) {
+            // top right
+            targetAngle = angle + 90;
+        }
+        return Math.floor(targetAngle);
+    }
 }
 class Debug {
     static print(value) {
@@ -65,7 +126,7 @@ class Debug {
     }
 }
 var raceInfo = new RaceInfo();
-var podTracking = new PodTracking();
+var podTracking;
 raceInfo.laps = parseInt(readline());
 var checkpointCount = parseInt(readline());
 for (var i = 0; i < checkpointCount; i++) {
@@ -74,33 +135,26 @@ for (var i = 0; i < checkpointCount; i++) {
     var checkpointY = inputs[1];
     raceInfo.checkPoints.push(new CheckPoint(i, checkpointX, checkpointY));
 }
-podTracking.myPods = [new Pod('0', '0'), new Pod('0', '0')];
-podTracking.enemyPods = [new Pod('0', '0'), new Pod('0', '0')];
+raceInfo.checkPointCount = raceInfo.checkPoints.length;
+raceInfo.lastCheckPointId = raceInfo.checkPoints.length - 1;
 // game loop
 while (true) {
-    for (var i = 0; i < 2; i++) {
-        var inputs = readline().split(' ');
-        podTracking.myPods[i].positionX = parseInt(inputs[0]);
-        podTracking.myPods[i].positionY = parseInt(inputs[1]);
-        podTracking.myPods[i].speedX = parseInt(inputs[2]);
-        podTracking.myPods[i].speedY = parseInt(inputs[3]);
-        podTracking.myPods[i].angle = parseInt(inputs[4]);
-        podTracking.myPods[i].nextCheckPointId = parseInt(inputs[5]);
+    raceInfo.frames++;
+    podTracking = HelperMethods.initializePods();
+    var paco = podTracking.myPods[0];
+    var megan = podTracking.myPods[1];
+    var pacoThrust = 100;
+    var meganThrust = 100;
+    if (!paco.hasTargetInFront(raceInfo.nextCheckPoint(paco))) {
+        pacoThrust = 0;
     }
-    for (var i = 0; i < 2; i++) {
-        var inputs = readline().split(' ');
-        podTracking.enemyPods[i].positionX = parseInt(inputs[0]);
-        podTracking.enemyPods[i].positionY = parseInt(inputs[1]);
-        podTracking.enemyPods[i].speedX = parseInt(inputs[2]);
-        podTracking.enemyPods[i].speedY = parseInt(inputs[3]);
-        podTracking.enemyPods[i].angle = parseInt(inputs[4]);
-        podTracking.enemyPods[i].nextCheckPointId = parseInt(inputs[5]);
+    if (raceInfo.frames === 1) {
+        pacoThrust = 'BOOST';
     }
-    // Write an action using print()
-    // To debug: printErr('Debug messages...');
-    // You have to output the target position
-    // followed by the power (0 <= thrust <= 100)
-    // i.e.: "x y thrust"
-    print('8000 4500 100');
-    print('8000 4500 100');
+    if (!megan.hasTargetInFront(podTracking.enemyPods[1])) {
+        meganThrust = 50;
+    }
+    var distance = Math.floor(HelperMethods.getDistanceBetween(megan, podTracking.enemyPods[1]));
+    paco.moveToPoint(raceInfo.nextCheckPoint(paco).positionX, raceInfo.nextCheckPoint(paco).positionY, pacoThrust);
+    megan.moveToPoint((podTracking.enemyPods[1].positionX + podTracking.enemyPods[1].speedX * 10), (podTracking.enemyPods[1].positionY + podTracking.enemyPods[1].speedY * 10), meganThrust);
 }
--- goldPodRacing.ts
+++ goldPodRacing.ts
@@ -1,8 +1,18 @@
 ///<reference path="definitions.d.ts" />
 
 class RaceInfo {
+    frames: number = 0;
     laps: number = 0;
     checkPoints: CheckPoint[] = [];
+    checkPointCount: number = 0;
+    lastCheckPointId: number = 0;
+    checkPoint(index: number) {
+        return this.checkPoints[index];
+    }
+    nextCheckPoint(pod: Pod) {
+        return this.checkPoints[pod.nextCheckPointId];
+    }
+
 }
 
 class Point {
@@ -19,13 +29,31 @@ class PodTracking {
     enemyPods: Pod[] = [];
 }
 class Pod extends Point {
+    id: number = 0;
     speedX: number = 0;
     speedY: number = 0;
+    boundDelta: number = 90;
     angle: number = 0;
     nextCheckPointId: number = 0;
 
+    constructor(id: number, x: string = '0', y: string = '0', sx: string = '0', sy: string = '0', angle: string = '0', nextId = '0') {
+        super(x, y);
+        this.id = id;
+        this.speedX = parseInt(sx);
+        this.speedY = parseInt(sy);
+        this.angle = parseInt(angle);
+        this.nextCheckPointId = parseInt(nextId);
+        // this.setVisibleRange();
+    }
+
     moveToPoint(x: number, y: number, thrust: number | string) {
         return print(`${x} ${y} ${thrust}`);
+    }
+
+    hasTargetInFront(target: CheckPoint | Pod): boolean {
+        var targetAngle = HelperMethods.getRelativeAngle(this, target) + 360;
+        var anglediff = (this.angle - targetAngle + 180 + 360) % 360 - 180
+        return (anglediff <= this.boundDelta && anglediff >= -this.boundDelta)
     }
 };
 
@@ -49,6 +77,18 @@ class NextCheckPoint extends Point {
 };
 
 class HelperMethods {
+    static initializePods(): PodTracking {
+        var podTracking = new PodTracking();
+        for (var i = 0; i < 2; i++) {
+            var inputs = readline().split(' ');
+            podTracking.myPods.push(new Pod(i, inputs[0], inputs[1], inputs[2], inputs[3], inputs[4], inputs[5]))
+        }
+        for (var i = 0; i < 2; i++) {
+            var inputs = readline().split(' ');
+            podTracking.enemyPods.push(new Pod(i + 2, inputs[0], inputs[1], inputs[2], inputs[3], inputs[4], inputs[5]))
+        }
+        return podTracking;
+    }
     static getDistanceBetween(pod1: Pod | CheckPoint, pod2: Pod | CheckPoint) {
         var a = pod1.positionX - pod2.positionX;
         var b = pod1.positionY - pod2.positionY;
@@ -60,6 +100,31 @@ class HelperMethods {
         var angleOfAttack = Math.atan(checkPointRadius / distanceToCheckpoint) * (180 / Math.PI);
         return angleOfAttack;
     }
+    static getRelativeAngle(pod: Pod, target: CheckPoint | Pod): number {
+        var deltaX = pod.positionX - target.positionX;
+        var deltaY = pod.positionY - target.positionY;
+        var absDeltaX = Math.abs(pod.positionX - target.positionX);
+        var absDeltaY = Math.abs(pod.positionY - target.positionY);
+        // var angle = Math.atan(opposite / adjacent) * (180 / Math.PI);
+        var angle = Math.atan(absDeltaY / absDeltaX) * (180 / Math.PI);
+        var targetAngle = 0;
+
+        if (deltaX < 0 && deltaY < 0) {
+            // top left
+            targetAngle = angle;
+        } else if (deltaX < 0 && deltaY > 0) {
+            // bottom left
+            targetAngle = angle + 270;
+        } else if (deltaX > 0 && deltaY > 0) {
+            // bottom right
+            targetAngle = angle + 180;
+        } else if (deltaX > 0 && deltaY < 0) {
+            // top right
+            targetAngle = angle + 90;
+        }
+
+        return Math.floor(targetAngle);
+    }
 }
 
 class Debug {
@@ -69,7 +134,7 @@ class Debug {
 }
 
 var raceInfo = new RaceInfo();
-var podTracking = new PodTracking();
+var podTracking: PodTracking;
 raceInfo.laps = parseInt(readline());
 var checkpointCount = parseInt(readline());
 for (var i = 0; i < checkpointCount; i++) {
@@ -78,40 +143,35 @@ for (var i = 0; i < checkpointCount; i++) {
     var checkpointY = inputs[1];
     raceInfo.checkPoints.push(new CheckPoint(i, checkpointX, checkpointY));
 }
-
-podTracking.myPods = [new Pod('0','0'), new Pod('0','0')];
-podTracking.enemyPods = [new Pod('0','0'), new Pod('0','0')];
+raceInfo.checkPointCount = raceInfo.checkPoints.length;
+raceInfo.lastCheckPointId = raceInfo.checkPoints.length - 1;
 
 // game loop
 while (true) {
-    for (var i = 0; i < 2; i++) {
-        var inputs = readline().split(' ');
-        podTracking.myPods[i].positionX = parseInt(inputs[0]);
-        podTracking.myPods[i].positionY = parseInt(inputs[1]);
-        podTracking.myPods[i].speedX = parseInt(inputs[2]);
-        podTracking.myPods[i].speedY = parseInt(inputs[3]);
-        podTracking.myPods[i].angle = parseInt(inputs[4]);
-        podTracking.myPods[i].nextCheckPointId = parseInt(inputs[5]);
-    }
-    for (var i = 0; i < 2; i++) {
-        var inputs = readline().split(' ');
-        podTracking.enemyPods[i].positionX = parseInt(inputs[0]);
-        podTracking.enemyPods[i].positionY = parseInt(inputs[1]);
-        podTracking.enemyPods[i].speedX = parseInt(inputs[2]);
-        podTracking.enemyPods[i].speedY = parseInt(inputs[3]);
-        podTracking.enemyPods[i].angle = parseInt(inputs[4]);
-        podTracking.enemyPods[i].nextCheckPointId = parseInt(inputs[5]);
+    raceInfo.frames++;
+    podTracking = HelperMethods.initializePods();
+
+    var paco = podTracking.myPods[0];
+    var megan = podTracking.myPods[1];
+
+    var pacoThrust:number|string = 100;
+    var meganThrust:number|string = 100;
+
+    if (!paco.hasTargetInFront(raceInfo.nextCheckPoint(paco))) {
+        pacoThrust = 0;
     }
 
-    // Write an action using print()
-    // To debug: printErr('Debug messages...');
+    if (raceInfo.frames === 1){
+        pacoThrust = 'BOOST';
+    }
 
+    
+    if (!megan.hasTargetInFront(podTracking.enemyPods[1])) {
+        meganThrust = 50;
+    }
 
-    // You have to output the target position
-    // followed by the power (0 <= thrust <= 100)
-    // i.e.: "x y thrust"
-    print('8000 4500 100');
-    print('8000 4500 100');
+    var distance =  Math.floor(HelperMethods.getDistanceBetween(megan, podTracking.enemyPods[1]));
+
+    paco.moveToPoint(raceInfo.nextCheckPoint(paco).positionX, raceInfo.nextCheckPoint(paco).positionY, pacoThrust);
+    megan.moveToPoint((podTracking.enemyPods[1].positionX + podTracking.enemyPods[1].speedX * 10) , (podTracking.enemyPods[1].positionY + podTracking.enemyPods[1].speedY * 10) , meganThrust);
 }
-
-
